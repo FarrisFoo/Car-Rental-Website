cac3368806e502a07ad6183f9eebeac296f358b8
FarrisFoo <foo.farris@gmail.com>
2021-10-27 16:43:49 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,496 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet"/>
+  <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet"/>
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.6.0/mdb.min.css" rel="stylesheet"/>
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="https://raw.githubusercontent.com/daneden/animate.css/master/animate.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500" rel="stylesheet" />
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/css/lightbox.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/js/lightbox.min.js"></script>
+    <link rel="stylesheet" href="stylesheet.css">
+    <title>Al Car Rental | Car Rental In West Malaysia</title>
+    <meta name="description" content="Al Car Rental provides car rentals all around West Malaysia, with over 50 cars in option. We wish to deliver comfortable and simple service to our customers, with hope to maintain great customer relationships...">
+</head>
+<body>
+    <!--START OF NAVIGATION BAR      START OF NAVIGATION BAR     START OF NAVIGATION BAR-->
+    <nav class="navbar navbar-expand-md navbar-light bg-light">
+      <a href="index.php" class="navbar-brand">
+          <img src="image/logo.png" height="60">AL Car Rental
+      </a>
+      <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+          <span class="navbar-toggler-icon"></span>
+      </button>
+  
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+          <div class="navbar-nav">
+              <a href="index.php" class="nav-item nav-link active">HOME</a>
+              <a href="car.html" class="nav-item nav-link">CAR</a>
+              <a href="about.html" class="nav-item nav-link">ABOUT</a>
+              <a href="contact.php" class="nav-item nav-link">CONTACT</a>
+          </div>
+      </div>
+  </nav>
+    <!--END OF NAVIGATION BAR      END OF NAVIGATION BAR     END OF NAVIGATION BAR-->
+     
+    <!-- START OF HEADER     START OF HEADER     START OF HEADER-->
+    <section class="hmain">
+      <div class="hcontent">
+        <div class="htext">
+          <h2>Have a nice ride<br>with<span> Al Car Rental</span></h2>
+          <P>We provide car rentals, based in Johor bahru with more than 50 cars ready to be rented at any time of the year.</P>
+          <a href="car.html" class="hbtn">Book Now</a>
+        </div>
+        <div class="hslider">
+          <div class="hslides active">
+            <img src="image/bezzadisplay.png">
+          </div>
+          <div class="hslides">
+            <img src="image/vellfirehitamdisplay.jpg.png">
+          </div>
+          <div class="hslides">
+            <img src="image/myvibirubarudisplay.png">
+          </div>
+        </div>
+      </div>
+      <div class="hfooter">
+        <ul class="hsci">
+          <li><a href="https://www.facebook.com/profile.php?id=100013350733686"><ion-icon name="logo-facebook"></ion-icon></a></li>
+        </ul>
+        <div class="hprevnext">
+          <span class="hprev"><ion-icon name="chevron-back-outline"></ion-icon></span>
+          <span class="hnext"><ion-icon name="chevron-forward-outline"></ion-icon></span>
+        </div>
+      </div>
+
+    </section>
+    <br>
+    <!-- END OF HEADER     END OF HEADER     END OF HEADER-->
+    <!--START OF SEARCH BAR     START OF SEARCH BAR     START OF SEARCH BOX-->
+    <div class="booksearchbox">
+      <div class="bookinputbox">
+        <p>Enter Car</p>
+        <input type="text" placeholder="Ex: Perodua Axia">
+      </div>
+      <div class="bookinputbox">
+        <p>Pick Up Date</p>
+        <input type="date">
+      </div>
+      <div class="bookinputbox">
+        <p>Return Date</p>
+        <input type="date">
+      </div>
+      <div class="bookinputbox">
+        <p class="nonesense">_</p>
+        <a href="booking.php"><input type="submit" value="Book"></a>
+      </div>
+    </div>
+    <!--END OF SEARCH BAR     END OF SEARCH BAR     END OF SEARCH BAR-->
+    <br>
+    <!--START OF STEPPER     START OF STEPPER     START OF STEPPER-->
+    <div class="dstepper">
+      <ul>
+        <li>
+          <img src="image/bookingicon.png"><br>
+          <i class="fa fa-check"></i>
+          <p>Book Any Car</p>
+        </li>
+        <li>
+          <img src="image/dealingicon.png"><br>
+          <i class="fa fa-check"></i>
+          <p>Deal Confirmation</p>
+        </li>
+        <li>
+          <img src="image/payingicon.png"><br>
+          <i class="fa fa-check"></i>
+          <p>Pay The Rent</p>
+        </li>
+        <li>
+          <img src="image/carrenticon.png"><br>
+          <i class="fa fa-check"></i>
+          <p>Deliver The Car</p>
+        </li>
+      </ul>
+    </div>
+    <br><br>
+    <!--END OF STEPPER     END OF STEPPER     END OF STEPPER-->
+    <!--END OF PRODUCT DISPLAY     END OF PRODUCT DISPLAY     END OF PRODUCT DISPLAY-->
+    <style>
+      .wrapper{
+margin: 100px auto;
+max-width: 1100px;
+}
+.wrapper nav{
+display: flex;
+justify-content: center;
+}
+.wrapper .items{
+display: flex;
+max-width: 720px;
+width: 100%;
+justify-content: space-between;
+}
+.items span{
+padding: 7px 25px;
+font-size: 18px;
+font-weight: 500;
+margin-left: -200px;
+cursor: pointer;
+color: #6b4900;
+border-radius: 50px;
+border: 2px solid #6b4900;
+transition: all 0.3s ease;
+}
+.items span.active,
+.items span:hover{
+color: #fff;
+background: #6b4900;
+}
+.gallery{
+display: flex;
+flex-wrap: wrap;
+margin-top: 30px;
+}
+.gallery .image{
+width: calc(100% / 4);
+padding: 7px;
+}
+.gallery .image span{
+display: flex;
+width: 100%;
+overflow: hidden;
+}
+.gallery .image img{
+width: 100%;
+vertical-align: middle;
+transition: all 0.3s ease;
+}
+.gallery .image:hover img{
+transform: scale(1.1);
+}
+.gallery .image.hide{
+display: none;
+}
+.gallery .image.show{
+animation: animate 0.4s ease;
+}
+@keyframes animate {
+0%{
+  transform: scale(0.5);
+}
+100%{
+  transform: scale(1);
+}
+}
+.preview-box{
+position: fixed;
+top: 50%;
+left: 50%;
+transform: translate(-50%, -50%) scale(0.9);
+background: #fff;
+max-width: 700px;
+width: 100%;
+z-index: 5;
+opacity: 0;
+pointer-events: none;
+border-radius: 3px;
+padding: 0 5px 5px 5px;
+box-shadow: 0px 0px 15px rgba(0,0,0,0.2);
+}
+.preview-box.show{
+opacity: 1;
+pointer-events: auto;
+transform: translate(-50%, -50%) scale(1);
+transition: all 0.3s ease;
+}
+.preview-box .details{
+padding: 13px 15px 13px 10px;
+display: flex;
+align-items: center;
+justify-content: space-between;
+}
+.details .title{
+display: flex;
+font-size: 18px;
+font-weight: 400;
+}
+.details .title p{
+font-weight: 500;
+margin-left: 5px;
+}
+.details .icon{
+color: #007bff;
+font-style: 22px;
+cursor: pointer;
+}
+.preview-box .image-box{
+width: 100%;
+display: flex;
+}
+.image-box img{
+width: 100%;
+border-radius: 0 0 3px 3px;
+}
+.shadow{
+position: fixed;
+left: 0;
+top: 0;
+height: 100%;
+width: 100%;
+z-index: 2;
+display: none;
+background: rgba(0,0,0,0.4);
+}
+.shadow.show{
+display: block;
+}
+@media (max-width: 1000px) {
+.gallery .image{
+  width: calc(100% / 3);
+}
+}
+@media (max-width: 800px) {
+.gallery .image{
+  width: calc(100% / 2);
+}
+}
+@media (max-width: 700px) {
+.wrapper nav .items{
+  max-width: 600px;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+nav .items span{
+  padding: 7px 15px;
+  font-size: 14px;
+  margin: 5px;
+  margin-left: 30px;
+  font-size: 16px;
+}
+nav .items span:first-child{
+  margin-right: -30px;
+}
+}
+@media (max-width: 600px) {
+.wrapper{
+  margin: 30px auto;
+}
+.wrapper nav .items{
+  flex-wrap: wrap;
+  justify-content: center;
+}
+nav .items span{
+  margin: 5px;
+  margin-left: 0px;
+  font-size: 14px;
+}
+nav .items span:first-child{
+  margin-right: -30px;
+}
+.gallery .image{
+  width: 100%;
+}
+}
+    </style>
+    <div class="wrapper">
+      <!-- filter Items -->
+      <nav>
+        <div class="items">
+          <span class="item active" data-name="all"></span>
+          <span class="item" data-name="delivery">Car Delivery</span>
+          <span class="item" data-name="whatsapp">Customer's Feedback</span>
+
+        </div>
+      </nav>
+      <!-- filter Images -->
+      <div class="gallery">
+        <div class="image" data-name="delivery"><span><img src="image/delivery1.png" alt=""></span></div>
+        <div class="image" data-name="delivery"><span><img src="image/delivery2.png" alt=""></span></div>
+        <div class="image" data-name="delivery"><span><img src="image/delivery3.png" alt=""></span></div>
+        <div class="image" data-name="delivery"><span><img src="image/delivery4.png" alt=""></span></div>
+        <div class="image" data-name="delivery"><span><img src="image/delivery5.PNG" alt=""></span></div>
+        <div class="image" data-name="delivery"><span><img src="image/delivery6.jpg" alt=""></span></div>
+        <div class="image" data-name="delivery"><span><img src="image/delivery7.jpg" alt=""></span></div>
+        <div class="image" data-name="delivery"><span><img src="image/delivery8.JPG" alt=""></span></div>
+        <div class="image" data-name="delivery"><span><img src="image/delivery9.JPG" alt=""></span></div>
+        <div class="image" data-name="delivery"><span><img src="image/delivery10.JPG" alt=""></span></div>
+        <div class="image" data-name="delivery"><span><img src="image/delivery11.JPG" alt=""></span></div>
+        <div class="image" data-name="delivery"><span><img src="image/delivery12.JPG" alt=""></span></div>
+        <div class="image" data-name="whatsapp"><span><img src="image/ws1.png" alt=""></span></div>
+        <div class="image" data-name="whatsapp"><span><img src="image/ws2.PNG" alt=""></span></div>
+        <div class="image" data-name="whatsapp"><span><img src="image/ws3.png" alt=""></span></div>
+        <div class="image" data-name="whatsapp"><span><img src="image/ws4.png" alt=""></span></div>
+        <div class="image" data-name="whatsapp"><span><img src="image/ws5.png" alt=""></span></div>
+      </div>
+    </div>
+    <!-- fullscreen img preview box -->
+    <div class="preview-box">
+      <div class="details">
+        <span class="title">Image Category: <p></p></span>
+        <span class="icon fas fa-times"></span>
+      </div>
+      <div class="image-box"><img src="" alt=""></div>
+    </div>
+    <div class="shadow"></div>
+    <!--START OF FEEDBACK     START OF FEEDBACK     START OF FEEDBACK-->
+
+    <!--START OF PRODUCT DISPLAY     START OF PRODUCT DISPLAY     START OF PRODUCT DISPLAY-->
+    <div class="lightbox-gallery">
+      <div class="container">
+        <div class="row photos">
+          <div class="col-sm-6 col-md-4 col-lg-3 item"><img style="margin-top: 40px;" class="img-fluid" src="image/myvi biru baru.png"></a></div>
+          <div class="col-sm-6 col-md-4 col-lg-3 item"><img class="img-fluid" src="image/bezza.jpg"></a></div>
+          <div class="col-sm-6 col-md-4 col-lg-3 item"><img style="margin-top: 10px;"class="img-fluid" src="image/starrex putih.jfif.png"></a></div>
+          <div class="col-sm-6 col-md-4 col-lg-3 item"><img style="margin-top: 30px;" class="img-fluid" src="image/vellfirehitam.png"></a></div>
+        </div>
+      </div>
+    </div>
+    <br>
+
+      <!--END OF FEEDBACK     END OF FEEDBACK     END OF FEEDBACK-->   
+    <!-- START OF FOOTER    START OF FOOTER    START OF FOOTER -->,,
+<footer class="text-center text-lg-start bg-light text-muted">
+  <!-- Section: Social media -->
+
+  <!-- Section: Links  -->
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <!-- Content -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            <i class="fas fa-gem me-3"></i>AL Car Rental
+          </h6>
+          <p>
+            Al Car Rental provides car rentals all around West Malaysia, with over 50 cars in option. We wish to deliver comfortable and simple service to our customers, with hope to maintain great customer relationships.
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Car
+          </h6>
+          <p>
+            <a href="car.html" class="text-reset">Regular</a>
+          </p>
+          <p>
+            <a href="car.html" class="text-reset">Sedan</a>
+          </p>
+          <p>
+            <a href="car.html" class="text-reset">MPV</a>
+          </p>
+          <p>
+            <a href="car.html" class="text-reset">SUV</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Useful link
+          </h6>
+          <p>
+            <a href="index.php" class="text-reset">Home</a>
+          </p>
+          <p>
+            <a href="car.html" class="text-reset">Car Selection</a>
+          </p>
+          <p>
+            <a href="about.html" class="text-reset">About</a>
+          </p>
+          <p>
+            <a href="contact.php" class="text-reset">Contact</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Contact
+          </h6>
+          <p><i class="fas fa-home me-3"></i>Pasir Gudang, Johor</p>
+          <p>
+            <i class="fas fa-envelope me-3"></i>
+            alhazril@yahoo.com
+          </p>
+          <p><i class="fas fa-phone me-3"></i>014-515 6050</p>
+          <p><i class="fab fa-whatsapp me-3"></i>014-515 6050</p>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+    © 2021 Copyright:
+    <a class="text-reset fw-bold" href="https://mdbootstrap.com/">alkeretasewapasirgudang.com</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- END OF FOOTER      END OF FOOTER    END OF FOOTER -->
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+<script src="script.js"></script>
+<script>
+const hslides = document.querySelectorAll('.hslides');
+const prev = document.querySelector('.hprev');
+const next = document.querySelector('.hnext');
+i = 0;
+
+function ActiveSlide(n){
+  for(slide of hslides)
+  slide.classList.remove('active');
+  hslides[n].classList.add('active');
+}
+
+// for prev btn
+prev.addEventListener('click', function(){
+  if(i == 0){
+    i = hslides.length - 1;
+    ActiveSlide(i);
+  }
+  else{
+    i--;
+    ActiveSlide(i);
+  }
+})
+
+// for next btn
+next.addEventListener('click', function(){
+  if(i == hslides.length - 1){
+    i = 0;
+    ActiveSlide(i);
+  }
+  else{
+    i++;
+    ActiveSlide(i);
+  }
+})
+
+</script>
+</body>
+</html>
